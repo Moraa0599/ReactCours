f761ed32a45cfae3ba74e450ee536ba342195053
moraa0599 <morala@uw.edu>
2018-10-20 12:33:50 -0700

    add char component

--- Assignment2/challenge2/src/validationcomponent/validation.js
+++ Assignment2/challenge2/src/validationcomponent/validation.js
@@ -3,7 +3,9 @@ import React from 'react'
 const validation = (props) => {
     return (
         <div>
-            <p>hi bro {props.paragraphlength}</p>
+            <p> 
+            {props.paragraphlength < 5 ? "This is too short" : 'this is too long' }
+            </p>
         </div>
        
     );
