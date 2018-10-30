21a60f8a0ea043976d23562b5f64e0ad39775133
moraa0599 <morala@uw.edu>
2018-10-29 20:20:31 -0700

    Added switch statment and modifed Burgeringredients.js

--- BurgerBuilder/burgerbuilder/src/components/Buger/Burgeringredients/Burgeringredients.js
+++ BurgerBuilder/burgerbuilder/src/components/Buger/Burgeringredients/Burgeringredients.js
@@ -1,15 +1,44 @@
 import React from 'react'
+import PropType from 'prop-type'
+import classes from './Bugeringredients.css'
 
-import classes from '../../Buger'
+class BurgerIngredients extends Component {
+    render() {
+        let ingredient = null;
 
-const burgerIngredients = (props) => {
-    let ingredients = null;
-
-    switch(props.type) {
-        case ('bread-bottom'):
-            ingredient = <div className={classes.BreadBottom}></div>;
+        switch(this.props.type) {
+            case ('bread-bottom'):
+                ingredient = <div className={classes.BreadBottom}></div>;
+                break;
+            case('bread-top'):
+                ingredient = (
+                    <div className={classes.BreadTop}>
+                        <div className={classes.Seeds1}></div>
+                        <div className={classes.Seeds2}></div>
+                    </div>
+                );
+                break;
+            case('meat'):
+                    ingredient = <dic className={classes.Meat}></dic>;
+                    break;
+            case('cheese'):
+                    ingredient = <dic className={classes.Cheese}></dic>;
+                    break;
+            case('bacon'):
+                    ingredient = <dic className={classes.Bacon}></dic>;
+                    break;
+            case('salad'):
+                    ingredient = <dic className={classes.Salad}></dic>;
+                    break;
+            default:
+            ingredient = null;
+        }
+        return ingredient;
     }
+}
+
+BurgerIngredients.PropType = {
+    type:PropType.string.isRequired
 };
 
-
-export default burgerIngredients;
+export default BurgerIngredients;
